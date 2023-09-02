90dc980122937a5ccc3f9d53005ee0e7525450a6
commandblock123 <88826536+commandblock123@users.noreply.github.com>
2023-09-02 14:12:13 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+
+os.system("sudo apt update")
+os.system("sudo apt install docker")
+
+os.system("docker pull tensorflow/tensorflow:1.6.0-gpu-py3")
+os.system("docker run -it tensorflow/tensorflow:1.6.0-gpu-py3 bash")
+
+os.system("sudo apt install python")
+os.system("pip install -r requirements.txt")
