8e28c903c1e54caa47a7c5332477f25960813473
commandblock123 <88826536+commandblock123@users.noreply.github.com>
2023-09-02 14:39:27 +0200

    Update and rename setup.py to setup_tensorflow.py

--- setup_tensorflow.py
+++ setup_tensorflow.py
@@ -5,6 +5,3 @@ os.system("sudo apt install docker")
 
 os.system("docker pull tensorflow/tensorflow:1.6.0-gpu-py3")
 os.system("docker run -it tensorflow/tensorflow:1.6.0-gpu-py3 bash")
-
-os.system("sudo apt install python")
-os.system("pip install -r requirements.txt")
