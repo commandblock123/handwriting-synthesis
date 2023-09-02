d5590273e8f43191464d1b0d56162244d7880eda
commandblock123 <88826536+commandblock123@users.noreply.github.com>
2023-09-02 16:32:55 +0200

    Create api.py

---
+++ api.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, request, jsonify, send_file
+
+app = Flask(__name__)
+
+@app.route('/synthesize_handwriting', methods=['POST'])
+def synthesize_handwriting():
+    try:
+        # Parse request data
+        data = request.json
+        lines = data.get('lines')
+        style = data.get('style')
+        bias = data.get('bias')
+        stroke_color = data.get('stroke_color')
+        stroke_width = data.get('stroke_width')
+        output_filename = data.get('output_filename')
+
+        # Run the handwriting synthesis script using os.system
+        command = f'python your_script.py -lines {" ".join(lines)} -style {style} -bias {bias} -stroke_color {stroke_color} -stroke_width {stroke_width} -output {output_filename}'
+
+
+        os.system(command)
+
+        # Return the generated SVG file
+        return send_file(output_filename, as_attachment=True, download_name='generated_handwriting.svg')
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
