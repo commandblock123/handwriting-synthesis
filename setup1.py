8c6309bc951788520503f8d8d2fb818b0e8e2795
commandblock123 <88826536+commandblock123@users.noreply.github.com>
2023-09-02 16:13:12 +0200

    Update setup1.py

--- setup1.py
+++ setup1.py
@@ -1,7 +1,7 @@
 import os
 
 os.system("sudo apt update")
-os.system("sudo apt install docker")
+os.system("sudo apt install docker -y")
 
 os.system("docker pull tensorflow/tensorflow:1.6.0-gpu-py3")
 os.system("docker run -it tensorflow/tensorflow:1.6.0-gpu-py3 bash")
