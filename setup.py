0f2b9b3a359baf9b2a24ef5673420b29f587a4c6
commandblock123 <88826536+commandblock123@users.noreply.github.com>
2023-09-02 14:41:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,3 +3,6 @@ import os
 os.system("sudo apt update")
 os.system("sudo apt install git")
 os.system("git clone https://github.com/commandblock123/handwriting-synthesis.git")
+
+os.system("cd handwriting-synthesis")
+os.system("python -m pip install -r requirements.txt")
