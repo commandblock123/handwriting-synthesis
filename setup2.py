b2443cef3070561394506d283e7c5a53a3ade56b
commandblock123 <88826536+commandblock123@users.noreply.github.com>
2024-05-12 13:06:29 +0200

    Update setup2.py

--- setup2.py
+++ setup2.py
@@ -2,5 +2,5 @@ import os
 
 os.system("apt update")
 
-os.system("python -m pip install -r requirements.txt")
+os.system("python -m pip install -r requirements.txt --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host=files.pythonhosted.org")
 os.system("apt install python3-tk -y")
