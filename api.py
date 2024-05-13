da5f31a243b1425c584edd3d3243e70e1b6eec59
commandblock123 <88826536+commandblock123@users.noreply.github.com>
2024-05-13 15:32:04 +0200

    fixed f strings not supported in python3.5

--- api.py
+++ api.py
@@ -17,12 +17,12 @@ def synthesize_handwriting():
         output_filename = "output.svg"
 
         # Run the handwriting synthesis script using os.system
-        command = f"python generate_handwriting.py -text {text} -style {style} -bias {bias} -stroke_color {stroke_color} -stroke_width {stroke_width} -output {output_filename}"
+        command = "python generate_handwriting.py -text {} -style {} -bias {} -stroke_color {} -stroke_width {} -output {}".format(text, style, bias, stroke_color, stroke_width, output_filename)
 
         os.system(command)
 
         # Return the generated SVG file
-        return send_file(f'{output_filename}.svg', as_attachment=True, download_name='generated_handwriting.svg')
+        return send_file('{}.svg'.format(output_filename), as_attachment=True, download_name='generated_handwriting.svg')
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
